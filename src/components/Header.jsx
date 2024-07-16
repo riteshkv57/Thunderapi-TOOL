7ea9761e9b6d442fb1950a3e8ae2f74c6c1b45f9
Ritesh verma <98600361+riteshkv57@users.noreply.github.com>
2024-07-17 00:52:56 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div >ThunderAPI-TOOL</div>
-        <div> <img src=".\public\flash.png" alt="" height={50} width={50}  /></div>
+        <div> <img src="/flash.png" alt="" height={50} width={50}  /></div>
     </div>
   )
 }
